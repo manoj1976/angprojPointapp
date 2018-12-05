50c60582374b307586c5658522a039d44da236ee
manoj1976 <manojbaby.ktr@gmail.com>
2018-12-05 22:39:05 +0000

    commit3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
+import { DataService } from './../services/data.service';
 import { Component } from '@angular/core';
-
+import { Services } from '@angular/core/src/view';
+import { from } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
   title = 'Point app';
+  constructor(private datasvc:DataService){
+
+  }
+
+  btnClick(){
+    this.datasvc.getTransactions();
+  }
+
 }
