bdfba62a84412070b8a7f723c80851b4a2f3dbf2
manoj1976 <manoj1976@users.noreply.github.com>
2018-12-09 11:55:11 +0000

    commit091218-1

--- src/test.ts
+++ src/test.ts
@@ -6,7 +6,6 @@ import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-
 declare const require: any;
 
 // First, initialize the Angular testing environment.
